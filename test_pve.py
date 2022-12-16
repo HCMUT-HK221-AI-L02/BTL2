6e3b25233725f7e4977f4eea4278f433f874982d
HiepNgo1609 <hiep.ngo1609@hcmut.edu.vn>
2022-12-16 14:30:03 +0700

    Hiep: pve

--- test_pve.py
+++ test_pve.py
@@ -2,6 +2,7 @@
 # Nếu một moveTupple không hợp lệ thì bắt chọn lại moveTupple
 
 # Import thư viện và hàm
+import random
 from app.state import *
 
 def main():
@@ -11,9 +12,40 @@ def main():
                     [1, 0, 0, 0, -1],
                     [-1, 0, 0, 0, -1],
                     [-1, -1, -1, -1, -1]]
-    master_board = State(start_board)
-    # Chọn ai đi trước và ai là 'X' hoặc 'O'
+    master_board = State(None, start_board)
+    # Chọn ai đi trước và ai là 'X' (-1) hoặc 'O' (1)
+    side = int(input("Input your side, 1 or -1: "))
         # Yêu cầu nhập xem ai là 'X' hoặc 'O'
+    turn = int(input("Who go first (1 | -1) ?"))
+
+    while True:
+        # User Turn
+        if turn == side:
+            isPossibleMove = False
+            while not isPossibleMove:
+                print("Please input a valid move.")
+                printCanPickPiece(master_board, turn)
+                startString = input("Input position of picked piece: ")
+                startTuple = eval(startString)
+                if printCanPickDes(master_board, startTuple): continue
+                endString = input("Input position of destination: ")
+                endTuple = eval(endString)
+                moveTuple = (startTuple, endTuple)
+                if master_board.boardMoveChk(moveTuple, side): 
+                    isPossibleMove = True
+        # Com turn
+        else:
+            # Random piece in pieceList
+            piece = random.choice(master_board.pieceList)
+            # Random move in pieceList.possibleMove
+            moveTuple = random.choice(piece.posibleMove)
+            return
+        master_board.boardMove(moveTuple)
+        # Check Victory
+        if master_board.victor:
+            print("End of game, the victory is " + str(side))
+            break
+        turn *= -1
         # Yêu cầu nhập xem ai đi trước
     # Tạo vòng lặp while, lần lượt người chơi thực hiện lượt đi
         # Nếu là Máy, gọi hàm move, trả ra tuple
