9a490c5e7cf56d2d61bcbadaa434301c3d30b454
HiepNgo1609 <hiep.ngo1609@hcmut.edu.vn>
2023-01-05 21:44:13 +0700

    Add Type go First and Fix Random turn

--- test_eve.py
+++ test_eve.py
@@ -15,32 +15,38 @@ def main():
     master_board = State(None, start_board)
 
     # MCST là 'X' (-1) và random 'O' (1)
-    turn = 1
+    turn = int(input("Who go first (1_(O) | -1_(X)) ?"))
+
     remain_time = {
         "remain_time_x": 20000,
         "remain_time_o": 20000
     }
 
     while True:
-        #MCST Turn
+        #MCST X(-1) Turn
         if turn == -1:
             # Hiện tại đang gọi hàm move, sẽ thay bằng hàm train MCST sau khi define xong
             moveTuple = move(master_board.prev_board, master_board.board, turn, remain_time["remain_time_x"], remain_time["remain_time_o"])
 
-        #Random Turn
+        #Random O(1) Turn
         else:
-            # Random piece in pieceList
-            piece = random.choice(master_board.pieceList)
-            # Random move in pieceList.possibleMove
+            # Lấy list các quân cờ thuộc phe Random
+            validPiece = []
+            for item in master_board.pieceList:
+                if item.team == turn and len(item.posibleMove) > 0:
+                    validPiece.append(item)
+            piece = random.choice(validPiece)
+            # Random move in validPiece
             moveTuple = random.choice(piece.posibleMove)
 
+        print(moveTuple)
         master_board.boardMove(moveTuple)
         # Viết file txt kết quả:
         nowBoard = master_board.board
         writeStateFile("test/eve.txt", nowBoard)
         # Kiểm tra thắng cuộc
         if master_board.victor:
-            side = "MCST" if turn == 1 else "Random"
+            side = "MCST" if turn == -1 else "Random"
             print("End of game, the victory is " + str(side))
             break
         turn *= -1
