74ffaa94d6673a6fbd117692018adcd8eb727998
HiepNgo1609 <hiep.ngo1609@hcmut.edu.vn>
2023-01-07 00:10:33 +0700

    4 Class Of Data

--- train_data_eve.py
+++ train_data_eve.py
@@ -14,6 +14,46 @@ def main():
             "train_details": []
         }
         json.dump(train_data, file)
+    # Opening Move Data (0-10 moves)
+    FILENAME_OPENING = 'traindata/opening.json'
+    with open(FILENAME_OPENING,'w') as file:
+        train_data = {
+            "train_details": []
+        }
+        json.dump(train_data, file)
+
+    # Middle Move Data (10-30 moves)
+    FILENAME_MIDDLE = 'traindata/middle.json'
+    with open(FILENAME_MIDDLE,'w') as file:
+        train_data = {
+            "train_details": []
+        }
+        json.dump(train_data, file)
+
+    # Middle Move Data (30-50 moves)
+    FILENAME_LATE = 'traindata/late.json'
+    with open(FILENAME_LATE,'w') as file:
+        train_data = {
+            "train_details": []
+        }
+        json.dump(train_data, file)
+
+    # Struggle Move Data (Khi số quân cờ đối thủ hơn phe mình)
+    FILENAME_STRUGGLE = 'traindata/struggle.json'
+    with open(FILENAME_STRUGGLE,'w') as file:
+        train_data = {
+            "train_details": []
+        }
+        json.dump(train_data, file)
+
+    # Close Out Move Data (Khi số quân cờ đối thủ hơn phe mình)
+    FILENAME_CLOSEOUT = 'traindata/closeout.json'
+    with open(FILENAME_LATE,'w') as file:
+        train_data = {
+            "train_details": []
+        }
+        json.dump(train_data, file)
+    
     for i in range(num_of_data):
         # Khởi tạo bàn cờ đầu tiên
         start_board = [[1, 1, 1, 1, 1],
@@ -54,15 +94,32 @@ def main():
                     "board": master_board.board,
                     "moveTuple": moveTuple
                 }
-                with open(FILENAME,'r+') as file:
-                    # First we load existing data into a dict.
-                    file_data = json.load(file)
-                    # Join new_data with file_data inside emp_details
-                    file_data["train_details"].append(new_data)
-                    # Sets file's current position at offset.
-                    file.seek(0)
-                    # convert back to json.
-                    json.dump(file_data, file)
+
+                if turn == 1:
+                    if remain_move["remain_move_o"] >= 40:
+                        appendData(FILENAME_OPENING, new_data)
+                    elif remain_move["remain_move_o"] >= 20:
+                        appendData(FILENAME_MIDDLE, new_data)
+                    else:
+                        appendData(FILENAME_LATE, new_data)
+                else:
+                    if remain_move["remain_move_x"] >= 40:
+                        appendData(FILENAME_OPENING, new_data)
+                    elif remain_move["remain_move_x"] >= 20:
+                        appendData(FILENAME_MIDDLE, new_data)
+                    else:
+                        appendData(FILENAME_LATE, new_data)
+
+                if master_board.advantageTeam() != 0 and master_board.advantageTeam() != side:
+                    appendData(FILENAME_STRUGGLE, new_data)
+
+                (O, X) = master_board.countPieceByTeam()
+                if side == 1 and O >= int(0.75*16):
+                    appendData(FILENAME_CLOSEOUT, new_data)
+                elif side == -1 and X >= int(0.75*16):
+                    appendData(FILENAME_CLOSEOUT, new_data)
+
+                
 
             #Random Turn
             else:
@@ -114,6 +171,18 @@ def main():
             printState(nowBoard)
 
             turn *= -1 
+
+def appendData(filename, newData):
+    with open(filename,'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["train_details"].append(newData)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file)
+
 
 if __name__ == "__main__":
     main()
